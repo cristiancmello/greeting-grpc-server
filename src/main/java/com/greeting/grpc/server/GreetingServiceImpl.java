4163c8d6c7e3f5276557309d833a26b762a01f33
Cristian Mello <cristianc.mello@gmail.com>
2022-07-24 16:31:13 -0300

    Updates...

--- src/main/java/com/greeting/grpc/server/GreetingServiceImpl.java
+++ src/main/java/com/greeting/grpc/server/GreetingServiceImpl.java
@@ -1,16 +1,31 @@
 package com.greeting.grpc.server;
 
 import io.grpc.stub.StreamObserver;
+
 import net.devh.boot.grpc.server.service.GrpcService;
+
+import com.greeting.grpc.gen.proto.GreetingServiceGrpc;
+import com.greeting.grpc.gen.proto.HelloRequest;
+import com.greeting.grpc.gen.proto.HelloResponse;
 
 @GrpcService
 public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
     @Override
     public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        String name = request.getName();
+        String dog = request.getDog();
+        String greeting = String.format("%s have a dog called %s", name, dog);
+        boolean loveDog = request.getILoveMyDog();
+
+        if (loveDog) {
+            greeting += " and ❤️ your dog";
+        }
+
         HelloResponse response = HelloResponse
                 .newBuilder()
-                .setGreeting("Hello from GRPC! Welcome " + request.getFirstName() + " " + request.getLastName())
+                .setGreeting(greeting)
                 .build();
+
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
